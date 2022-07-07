4b062b1d680e4a5eb1bd9010df3b53098060eff2
m-amirazmi <60350614+m-amirazmi@users.noreply.github.com>
2022-07-07 18:56:50 +0800

    for desktop

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ const animateMobile = () => {
 			trigger: 'main',
 			start: 'top 0%',
 			end: 'bottom 0%',
+			markers: true,
 			scrub: 1,
 			pin: true,
 		},
@@ -159,19 +160,132 @@ const animate = () => {
 			start: 'top 0%',
 			end: 'bottom 0%',
 			markers: true,
-			scrub: 50,
+			scrub: 1,
 			pin: true,
 		},
 	});
+
 	tl.to('#sun .sun-img', {
 		x: '-50vw',
-		duration: 10,
+		duration: 50,
+		ease: Power0.easeNone,
+	});
+
+	tl.fromTo(
+		'#intro .intro-text',
+		{
+			y: '40vh',
+			autoAlpha: 0,
+		},
+		{
+			y: 0,
+			duration: 50,
+			ease: Power0.easeNone,
+			autoAlpha: 1,
+		},
+		'<+0.5'
+	);
+
+	tl.to('#intro .intro-text', {
+		autoAlpha: 0,
+		y: '-20vh',
+		duration: 50,
+		ease: Power0.easeNone,
+	});
+
+	tl.to(
+		'#sun .sun-img',
+		{
+			x: 0,
+			delay: 10,
+			duration: 50,
+			ease: Power0.easeNone,
+		},
+		'<+2'
+	);
+
+	tl.to('#bg', {
+		y: '80vh',
+		duration: 20,
+		ease: Power0.easeNone,
+	});
+
+	tl.to('#bg', {
+		y: '-50vh',
+		duration: 50,
 		ease: Power0.easeNone,
 	});
 
 	tl.to('#sun .sun-img', {
-		x: 0,
-		duration: 10,
+		height: '320px',
+		y: '-100px',
+		duration: 30,
 		ease: Power0.easeNone,
+	});
+
+	tl.to(
+		'#inspired p',
+		{
+			autoAlpha: 1,
+			y: '50%',
+			duration: 30,
+			ease: Power0.easeNone,
+		},
+		'<+1'
+	);
+
+	tl.to('#bg', {
+		y: '-125vh',
+		duration: 70,
+		ease: Power0.easeNone,
+		delay: 30,
+	});
+
+	tl.to(
+		'#sun .sun-img',
+		{
+			y: '-120vh',
+			duration: 50,
+			ease: Power0.easeNone,
+		},
+		'<'
+	);
+
+	tl.to(
+		'#inspired p',
+		{
+			y: '-550%',
+			duration: 50,
+			ease: Power0.easeNone,
+		},
+		'<'
+	);
+
+	const introducing = document.querySelectorAll('.introducing');
+	introducing.forEach((intro, i) => {
+		const introTl = gsap.timeline();
+		introTl.fromTo(
+			intro,
+			{
+				autoAlpha: 0,
+				y: '30vh',
+			},
+			{
+				autoAlpha: 1,
+				y: 0,
+				duration: 50,
+				delay: 10,
+			},
+			'>+2'
+		);
+
+		introTl.to(intro, {
+			autoAlpha: 0,
+			y: '-10vh',
+			duration: 50,
+			delay: 30,
+		});
+
+		tl.add(introTl, '>+1.5');
 	});
 };
